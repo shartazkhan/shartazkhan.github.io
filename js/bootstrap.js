9086fd893a6fcfec67ab68c4233f1378889741c5
Shartaz khan akash <74089103+shartazkhan@users.noreply.github.com>
2021-10-03 12:22:47 +0600

    Add files via upload

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -5013,4 +5013,22 @@
   return index_umd;
 
 })));
+
+
+/* 2) Add JavaScript: */
+var i = 0;
+var txt = 'Lorem ipsum typing effect!'; /* The text */
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+
+
+
 //# sourceMappingURL=bootstrap.js.map
